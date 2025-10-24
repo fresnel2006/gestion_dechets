e31514c892672ab4e8ec9418aba413b125592f5c
DELL <traoreangefresnel@gmail.com>
2025-10-24 17:20:23 +0000

    Description de la mise à jour

--- lib/Pages/Drawer.dart
+++ lib/Pages/Drawer.dart
@@ -19,9 +19,8 @@ class _DrawerPageState extends State<DrawerPage> {
         controller: _controller,
         mainScreenTapClose: true,
         angle: 0,
-        slideWidth: 200,
+        slideWidth: MediaQuery.of(context).size.width *0.7,
         showShadow: true,
-
         shadowLayer1Color: Colors.lightGreen[200],
         shadowLayer2Color: Colors.green[100],
         menuScreen: MenuPage(controller: _controller), mainScreen: AcceuilPage(controller: _controller));
--- lib/Pages/Menu.dart
+++ lib/Pages/Menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_zoom_drawer/src/drawer_controller.dart';
 import 'package:lottie/lottie.dart';
 
@@ -13,21 +14,71 @@ class _MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       backgroundColor: Colors.lightGreen,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+spacing: MediaQuery.of(context).size.height *0.018,
         children: [
-        CircleAvatar(backgroundColor: Colors.white ,radius: 45,child: Lottie.asset("assets/animations/deploiement.json"),),
-          SizedBox(height: 20,),
-          Container(height: 40,
-            width: 150,color: Colors.white,),
-          SizedBox(height: 20,),
-          Container(height: 40,
-            width: 150,color: Colors.white,),
-          SizedBox(height: 20,),
-          Container(height: 40,
-            width: 150,color: Colors.white,)
-      ],),
+        Container(decoration:BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(MediaQuery.of(context).size.width *1)),border: Border.all(color: Colors.black)),child: CircleAvatar(backgroundColor: Colors.white ,radius: 45,child: Lottie.asset("assets/animations/deploiement.json"),),
+        ),SizedBox(height: MediaQuery.of(context).size.height *0.025,),
+          Container(
+            margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.03),height: MediaQuery.of(context).size.height *0.065,
+            decoration: BoxDecoration(border: Border.all(color: Colors.black),color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(MediaQuery.of(context).size.width *0.1))),
+            width: MediaQuery.of(context).size.width *1,
+          child:Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.account_circle_sharp),
+              TextButton(onPressed: (){}, child: Text("Mon compte",style: TextStyle(color: Colors.black)))
+            ],
+          )),
+          Container(
+              margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.03),height: MediaQuery.of(context).size.height *0.065,
+              decoration: BoxDecoration(border: Border.all(color: Colors.black),color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(MediaQuery.of(context).size.width *0.1))),
+              width: MediaQuery.of(context).size.width *1,
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.car_crash),
+                  TextButton(onPressed: (){}, child: Text("PASSER PRO",style: TextStyle(color: Colors.black),))
+                ],
+              )),
+          SizedBox(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+            Container(height: MediaQuery.of(context).size.height *0.002,width:MediaQuery.of(context).size.width *0.2,color: Colors.black,),
+            Text("OU"),
+            Container(height: MediaQuery.of(context).size.height *0.002,width:MediaQuery.of(context).size.width *0.2,color: Colors.black,),
+
+          ],),
+          SizedBox(),
+          Container(
+              margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.03),height: MediaQuery.of(context).size.height *0.065,
+              decoration: BoxDecoration(border: Border.all(color: Colors.black),color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(MediaQuery.of(context).size.width *0.1))),
+              width: MediaQuery.of(context).size.width *1,
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.add),
+                  TextButton(onPressed: (){}, child: Text("VOIR PLUS",style: TextStyle(color: Colors.black)))
+                ],
+              )),
+          Container(
+              margin: EdgeInsets.only(left: MediaQuery.of(context).size.width *0.03),height: MediaQuery.of(context).size.height *0.065,
+              decoration: BoxDecoration(border: Border.all(color: Colors.black),color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(MediaQuery.of(context).size.width *0.1))),
+              width: MediaQuery.of(context).size.width *1,
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.headset_mic_outlined),
+                  TextButton(onPressed: (){}, child: Text("ASSISTANCE",style: TextStyle(color: Colors.black)))
+                ],
+              )),
+
+
+        ],),
     );
   }
 }
